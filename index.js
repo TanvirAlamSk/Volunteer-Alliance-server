9e355ad9c8a59779212011f139a70be6ce678115
Tanvir Alam <ta89725@gmail.com>
2023-09-08 21:23:52 +0600

    start Volunteer Alliance project

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("server is running")
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on Port ${port}`)
+})
